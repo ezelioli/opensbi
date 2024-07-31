6eaa2ab12b1c1141fcbae53c8b34926600a1db1b
Enrico Zelioli <ezelioli@iis.ee.ethz.ch>
2024-07-31 09:54:15 +0200

    platform: add Cheshire platform target

---
+++ platform/fpga/cheshire/objects.mk
@@ -0,0 +1,15 @@
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# Copyright (C) 2019 FORTH-ICS/CARV
+#		Panagiotis Peristerakis <perister@ics.forth.gr>
+#
+
+# Compiler flags
+platform-cppflags-y =
+platform-cflags-y   =
+platform-asflags-y  =
+platform-ldflags-y  =
+
+# Object to build
+platform-objs-y += platform.o
