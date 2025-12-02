2dbe53e3038e83a83968e92ed57aaaa2b07ab146
Enrico Zelioli <ezelioli@iis.ee.ethz.ch>
2025-12-02 23:47:44 +0100

    treewide: add SBI CLIC extension

--- lib/sbi/sbi_hart.c
+++ lib/sbi/sbi_hart.c
@@ -688,6 +688,7 @@ const struct sbi_hart_ext_data sbi_hart_ext[] = {
 	__SBI_HART_EXT_DATA(zicfilp, SBI_HART_EXT_ZICFILP),
 	__SBI_HART_EXT_DATA(zicfiss, SBI_HART_EXT_ZICFISS),
 	__SBI_HART_EXT_DATA(ssdbltrp, SBI_HART_EXT_SSDBLTRP),
+	__SBI_HART_EXT_DATA(ssdbltrp, SBI_HART_EXT_CLIC),
 };
 
 _Static_assert(SBI_HART_EXT_MAX == array_size(sbi_hart_ext),
@@ -923,6 +924,9 @@ __pmp_skip:
 	/* Detect if hart has AIA local interrupt CSRs */
 	__check_ext_csr(SBI_HART_PRIV_VER_UNKNOWN,
 			CSR_MTOPI, SBI_HART_EXT_SMAIA);
+	/* Detect if hart has CLIC local interrupt CSRs */
+	__check_ext_csr(SBI_HART_PRIV_VER_UNKNOWN,
+			CSR_MTVT, SBI_HART_EXT_CLIC);
 	/* Detect if hart supports stimecmp CSR(Sstc extension) */
 	__check_ext_csr(SBI_HART_PRIV_VER_1_12,
 			CSR_STIMECMP, SBI_HART_EXT_SSTC);
--- lib/sbi/sbi_init.c
+++ lib/sbi/sbi_init.c
@@ -32,6 +32,7 @@
 #include <sbi/sbi_tlb.h>
 #include <sbi/sbi_version.h>
 #include <sbi/sbi_unit_test.h>
+#include <sbi_utils/irqchip/clic.h>
 
 #define BANNER                                              \
 	"   ____                    _____ ____ _____\n"     \
@@ -321,6 +322,13 @@ static void __noreturn init_coldboot(struct sbi_scratch *scratch, u32 hartid)
 		sbi_printf("%s: mpxy init failed (error %d)\n", __func__, rc);
 		sbi_hart_hang();
 	}
+
+	sbi_timer_event_start(0xfffffffffffffffful); // set next timer event to `never`
+	if(sbi_hart_has_extension(sbi_scratch_thishart_ptr(), SBI_HART_EXT_CLIC)){
+		clic_set_trigger(IRQ_M_TIMER, CLIC_INT_ATTR_TRIG_EDGE);
+		clic_set_priority(IRQ_M_TIMER, 0xFF);
+	}
+
 	/*
 	 * Note: Finalize domains after HSM initialization so that we
 	 * can startup non-root domains.
